bcdafbb8cc1566cfc09d1ef0a0e2ea919909af84
VanessaWendling <vanessamw2001@hotmail.com>
2024-09-18 13:18:59 -0300

    login page updated

--- tcc-frontend/src/Pages/Login.tsx
+++ tcc-frontend/src/Pages/Login.tsx
@@ -1,7 +1,9 @@
 import { SubmitHandler, useForm } from "react-hook-form";
-import LoginImage from "../assets/loginImage.jpg";
+import LoginImage from "../assets/loginImage_copy.jpg";
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { MdAlternateEmail, MdPassword } from "react-icons/md";
+import { Input } from "../Components/Input";
 
 type Inputs = {
   email: string;
@@ -22,53 +24,41 @@ export const Login = () => {
   console.log(watch("email")); // watch input value by passing the name of it
 
   return (
-    <div className="bg-pink-600  flex h-full justify-center items-center ">
-      <div className="max-w-[800px] md:w-[800px] bg-brown-200 flex flex-col m-auto rounded-md relative my-8">
-        <div className="absolute w-full h-full text-gray-200 max-h-[400px] sm:w-[240px] bg-black/40 flex flex-col">
-          <h1 className="px-4 text-2xl sm:text-2xl md:text-4xl lg:7xl font-bold pt-4">
-            Welcome to
-          </h1>
-          <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:8xl font-bold">
-            Bonna<span className="text-pink-900">Care</span>
-          </h1>
-        </div>
-        <img
-          className="max-h-[400px] md:w-[800px] sm:w-[240px] w-full object-cover rounded-t-xl"
-          src={LoginImage}
-        />
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col gap-4 p-4 items-center m-4"
-        >
-          <div className="flex flex-col">
-            <label>E-mail</label>
-            <input
-              {...register("email")}
-              className="p-2 rounded-2xl"
-              type="email"
-              placeholder="user@mail.com"
-            />
+    <div className="bg-amber-900 flex min-h-screen justify-center items-center">
+      <div className=" max-w-[1000px] max-h-[500px] md:h-[600px] bg-stone-100 flex flex-col-reverse md:flex-row m-auto rounded-md justify-between  my-8">
+        <div className="w-full md:w-1/2 flex flex-col justify-center align-baseline items-center p-4">
+          <div className="w-full text-gray-900 text-center">
+            <h1 className="px-4 text-2xl md:text-4xl font-bold pt-4">
+              Welcome to
+            </h1>
+            <h1 className="px-4 text-4xl md:text-6xl font-bold">
+              Bonna<span className="text-rose-800">Care</span>
+            </h1>
           </div>
-          <div className="flex flex-col">
-            <label>Password</label>
-            <input
-              {...register("password", { required: true })}
-              className="p-2 rounded-2xl"
-              type="password"
-              placeholder="********"
-            />
-            {errors.password && <span>This field is required</span>}
-          </div>
-          <button
-            type="submit"
-            className="border border-black rounded-xl px-5 py-1 max-w-32"
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-4 p-6 items-center w-full"
           >
-            Sign in
-          </button>
-        </form>
-        <h2 onClick={() => navigate("/register")} className="text-center pb-6 text-brown-900 cursor-pointer">
-          Join us
-        </h2>
+              <Input Icon={MdAlternateEmail} placeholder={"email"}/>
+              <Input Icon={MdPassword} placeholder={"********"}/>
+              {errors.password && <span>This field is required</span>}
+            <button
+              type="submit"
+              className="border border-black rounded-md px-5 py-1 max-w-32"
+            >
+              Sign in
+            </button>
+          </form>
+          <h2
+            onClick={() => navigate("/register")}
+            className="text-center pb-6 text-brown-900 cursor-pointer"
+          >
+            Join us
+          </h2>
+        </div>
+        <div className="w-full md:w-1/2 flex items-center justify-center  ">
+          <img className="object-cover h-full rounded-e-md" src={LoginImage} />
+        </div>
       </div>
     </div>
   );
