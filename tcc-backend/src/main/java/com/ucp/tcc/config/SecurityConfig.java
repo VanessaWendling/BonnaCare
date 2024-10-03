6e50d790119d6e009869753547137a671f586846
VanessaWendling <vanessamw2001@hotmail.com>
2024-10-02 22:04:21 -0300

    cors funcionando para o front

--- tcc-backend/src/main/java/com/ucp/tcc/config/SecurityConfig.java
+++ tcc-backend/src/main/java/com/ucp/tcc/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.ucp.tcc.config;
 
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -20,6 +21,9 @@ import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
@@ -35,17 +39,16 @@ public class SecurityConfig {
 	private RSAPublicKey key;
 	@Value("${jwt.private.key}")
 	private RSAPrivateKey priv;
-	
+
 	private UserDetailsService userDetailsService;
-	
+
 	public SecurityConfig(UserDetailsService userDetailsService) {
 		this.userDetailsService = userDetailsService;
 	}
 
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf(csrf -> csrf.disable()).cors(cors -> cors.disable()).authorizeHttpRequests(auth -> auth
-//						.anyRequest().permitAll())
+		http.csrf(csrf -> csrf.disable()).cors(Customizer.withDefaults()).authorizeHttpRequests(auth -> auth
 				.requestMatchers(HttpMethod.POST, "/login").permitAll().requestMatchers(HttpMethod.POST, "/person")
 				.permitAll().anyRequest().authenticated()).httpBasic(Customizer.withDefaults())
 				.oauth2ResourceServer(conf -> conf.jwt(jwt -> jwt.decoder(jwtDecoder())));
@@ -68,13 +71,25 @@ public class SecurityConfig {
 		JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
 		return new NimbusJwtEncoder(jwks);
 	}
-	
-	 @Bean
-	    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
-	        AuthenticationManagerBuilder authenticationManagerBuilder = 
-	            http.getSharedObject(AuthenticationManagerBuilder.class);
-	        authenticationManagerBuilder.userDetailsService(userDetailsService)
-	            .passwordEncoder(passwordEncoder());
-	        return authenticationManagerBuilder.build();
-	    }
+
+	@Bean
+	public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+		AuthenticationManagerBuilder authenticationManagerBuilder = http
+				.getSharedObject(AuthenticationManagerBuilder.class);
+		authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+		return authenticationManagerBuilder.build();
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Origem permitida
+		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Métodos permitidos
+		configuration.setAllowedHeaders(List.of("*")); // Permitir todos os headers
+		configuration.setAllowCredentials(true); // Permitir credenciais
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
 }
