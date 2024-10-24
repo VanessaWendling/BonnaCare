9a5f0ac8e9bb1ab9dcd2ce25aef08862dfd77956
VanessaWendling <vanessamw2001@hotmail.com>
2024-10-24 10:41:31 -0300

    switch language and design

--- tcc-frontend/src/Components/Cards/CardPetsDetails.tsx
+++ tcc-frontend/src/Components/Cards/CardPetsDetails.tsx
@@ -5,9 +5,9 @@ import { calculateAge, IAge } from "../../Utils/functions";
 import { Photo } from "../Photo";
 
 interface ICardDigsDetails {
-  name: string;
-  photo: string;
-  birthday: string;
+  name?: string;
+  photo?: string;
+  birthday?: string;
   idPet: string;
   localizator?: string;
   microchip?: string;
@@ -23,31 +23,44 @@ export const CardPetsDetails = ({
   microchip,
   role,
 }: ICardDigsDetails) => {
-  const age: IAge = calculateAge(birthday);
+  const age: IAge | undefined = birthday ? calculateAge(birthday) : undefined;
   return (
     <div className="bg-slate-50 shadow-lg min-h-[200px] rounded-2xl flex flex-row justify-evenly p-2 gap-4">
-      <div className="bg-yellow-200 rounded-2xl self-center justify-center flex items-center h-40 w-40">
+      <div className="bg-pink-900 rounded-2xl self-center justify-center flex items-center h-40 w-40">
         {photo ? <Photo photo={photo} /> : <PiDogLight size={80} />}
       </div>
       <div className="flex flex-col self-center">
         <h3 className="font-semibold text-xl text-center pb-3">{name}</h3>
-        <h3 className="text-center">
-          {age.years}y {age.months}months
-        </h3>
+
+        <div className="flex flex-row gap-1">
+          <h3 className="font-semibold">Idade:</h3>
+          {age && age.years > 0 && (
+            <h3>
+              {age.years}ano{age.years === 1 ? "" : "s"}
+            </h3>
+          )}
+          {age && (
+            <h3>
+              {age.months}m{age.months === 1 ? "ê" : "e"}s
+              {age.months === 1 ? "" : "es"}
+            </h3>
+          )}
+        </div>
+
         {microchip && (
           <div className="flex flex-row gap-1">
             <h3 className="font-semibold">Microchip:</h3>
             <h3>{microchip}</h3>
           </div>
         )}
-        {localizator && role == "ROLE_USER" && (
+        {localizator && role === "ROLE_USER" && (
           <div className="flex flex-row gap-1">
-          <h3 className="font-semibold">Localizator:</h3>
-          <h3>{localizator}</h3>
-        </div>
+            <h3 className="font-semibold">Localizador:</h3>
+            <h3>{localizator}</h3>
+          </div>
         )}
         <div className="flex-row flex w-full justify-around pt-4">
-          {role == "ROLE_USER" && <Button text="Edit" idPet={idPet} />}
+          {role === "ROLE_USER" && <Button text="Editar" idPet={idPet} />}
         </div>
       </div>
     </div>
